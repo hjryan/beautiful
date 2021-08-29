5ded7182d813d3197a9c569e64aa54b0a0a0fa9a
Hannah Ryan <hjryan@uw.edu>
2021-08-29 15:38:18 -0700

    Procfile etc

--- app.py
+++ app.py
@@ -4,6 +4,33 @@ import time
 from slack_bolt import App
 from yelpapi import YelpAPI
 
+########################################
+import logging
+
+#from slack_bolt import App
+
+logging.basicConfig(level=logging.DEBUG)
+app = App()
+
+
+@app.command("/hello-bolt-python-heroku")
+def hello(body, ack):
+    user_id = body["user_id"]
+    ack(f"Hi <@{user_id}>!")
+
+
+from flask import Flask, request
+from slack_bolt.adapter.flask import SlackRequestHandler
+
+flask_app = Flask(__name__)
+handler = SlackRequestHandler(app)
+
+
+@flask_app.route("/slack/events", methods=["POST"])
+def slack_events():
+    return handler.handle(request)
+########################################
+
 
 # initializes app with bot token and signing secret
 app = App(token=os.environ.get('SLACK_BOT_TOKEN'),
